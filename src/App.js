2f53295cd388ba931dc3f4cebccd97a45048bd47
Abin T S <abints30122004@gmail.com>
2025-10-04 19:23:51 +0530

    API Intergration is done and Dynamically rendered everything based on the category

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import NavBar from './Components/NavBar/NavBar';
 import './App.css'
+import {original,action,Horror} from './urls'
 import './Components/Banner/Banner.css'
 import Banner from './Components/Banner/Banner';
 import RawPost from './Components/RawPost/RawPost';
@@ -9,7 +10,9 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RawPost/>
+      <RawPost url={original} title='Netflix Originals' />
+      <RawPost url={action} title='Action Movies' isSmall/>
+      <RawPost url={Horror} title='Horror Movies' isSmall/>
     </div>
   );
 }
---
+++ src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '42f3b9d1ebb25481e726362111596b9f';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
